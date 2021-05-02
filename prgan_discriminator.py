b06691c519954166a3b3e6cb95983ea0a4ef4126
Amy Gao <amygao1997@gmail.com>
2021-05-02 13:23:56 -0400

    add evluation function

--- prgan_discriminator.py
+++ prgan_discriminator.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 
 class ShapeDiscriminator3D(torch.nn.Module):
@@ -12,19 +11,17 @@ class ShapeDiscriminator3D(torch.nn.Module):
 
         # Architecture
         self.batch_norm_1 = torch.nn.BatchNorm2d(128)
-        self.batch_norm_2 = torch.nn.BatchNorm2d(128*2)
-        self.batch_norm_3 = torch.nn.BatchNorm2d(128*4)
-        self.batch_norm_4 = torch.nn.BatchNorm2d(128*8)
-
+        self.batch_norm_2 = torch.nn.BatchNorm2d(128 * 2)
+        self.batch_norm_3 = torch.nn.BatchNorm2d(128 * 4)
+        self.batch_norm_4 = torch.nn.BatchNorm2d(128 * 8)
 
         self.conv2d_1 = torch.nn.Conv2d(1, self.d_size, 5, 2, 2)
-        self.conv2d_2 = torch.nn.Conv2d(self.d_size, self.d_size*2, 5, 2, 2)
+        self.conv2d_2 = torch.nn.Conv2d(self.d_size, self.d_size * 2, 5, 2, 2)
         self.conv2d_3 = torch.nn.Conv2d(self.d_size * 2, self.d_size * 4, 5, 2, 2)
         self.conv2d_4 = torch.nn.Conv2d(self.d_size * 4, self.d_size * 8, 5, 2, 2)
         self.linear = torch.nn.Linear(16384, 1)
         self.leakyrelu = torch.nn.LeakyReLU(0.2)
         self.sigmoid = torch.nn.Sigmoid()
-
 
     def forward(self, image):
         # input must be a torch.randn(64, 1, 32, 32)
@@ -33,7 +30,7 @@ class ShapeDiscriminator3D(torch.nn.Module):
         h0 = self.conv2d_1(reshaped_img)
         h0 = self.leakyrelu(self.batch_norm_1(h0))
 
-        h1= self.conv2d_2(h0)
+        h1 = self.conv2d_2(h0)
         h1 = self.leakyrelu(self.batch_norm_2(h1))
 
         h2 = self.conv2d_3(h1)
@@ -52,220 +49,21 @@ class ShapeDiscriminator3D(torch.nn.Module):
         return sigmoid
 
 
-
-
 def l2(a, b):
     return (torch.pow(a - b, 2)).mean()
 
 
-
 def loss_discriminator(real, fake):
-        #https://stackoverflow.com/questions/65458736/pytorch-equivalent-to-tf-nn-softmax-cross-entropy-with-logits-and-tf-nn-sigmoid
-
-        # calculate sigmoid_cross_entropy_with_logits
-        # not sure if this is right
-        #loss = p * -torch.log(logits) + (1 - p) * -torch.log(1 - logits)
-
-        loss = torch.square(real- torch.ones(real.shape))  + torch.square(fake)
-        # low gradient
-
-        return loss.mean()
-
-
-def loss_generator(fake):
     # https://stackoverflow.com/questions/65458736/pytorch-equivalent-to-tf-nn-softmax-cross-entropy-with-logits-and-tf-nn-sigmoid
 
     # calculate sigmoid_cross_entropy_with_logits
     # not sure if this is right
     # loss = p * -torch.log(logits) + (1 - p) * -torch.log(1 - logits)
 
-    loss = torch.square(fake - torch.ones(fake.shape))
+    loss = torch.square(real - torch.ones(real.shape)) + torch.square(fake)
     # low gradient
 
-    return loss.sum()
+    return loss.mean()
 
 def stat(input):
-        return input.mean(dim=2) , input.std(dim=2)
-###############################  test script begin
-# model = ShapeDiscriminator3D()
-# input = torch.randn(64, 1, 64, 64)
-# sigmoid = model(input, False)
-# p = torch.ones(sigmoid.shape)
-# loss = model.loss(h4, p)
-# mean, std = model.stat(h0)
-#
-#
-# G_loss = l2(dr_mean, dl_mean)
-# G_loss += l2(dr_var, dl_var)
-# D_loss = D_loss_real + D_loss_fake
-
-
-
-###############################  test script end
-# calculate the loss
-# write a scirpt that call both function
-## optimzie the code
-# render the image
-# 
-
-# self.G_loss = ops.l2(dr_mean, dl_mean)
-#
-# self.D_optim = tf.train.AdamOptimizer(1e-4, beta1=0.5).minimize(self.D_loss, var_list=self.D_vars)
-# self.G_optim = tf.train.AdamOptimizer(0.0025, beta1=0.5).minimize(self.G_loss, var_list=self.G_vars)
-# self.G_optim_classic = tf.train.AdamOptimizer(0.0025, beta1=0.5).minimize(self.G_loss_classic, var_list=self.G_vars)
-#
-#
-#
-# p_tensor=  tf.convert_to_tensor(torch.empty(sigmoid.shape))
-# logits_logit= tf.convert_to_tensor(torch.empty(logits .shape))
-#
-# logits_tensor = tf.convert_to_tensor(logits)
-#
-# p = p_tensor
-# logit = logits_logit
-#
-# loss = p*-torch.log(torch.sigmoid(logits)) + (1-p)*-torch.log(1-torch.sigmoid(logits))
-
-
-
-
-
-# #https://stackoverflow.com/questions/52975843/comparing-conv2d-with-padding-between-tensorflow-and-pytorch
-#
-#
-#
-#
-#         d_size = 128
-#         self.image_size = 32
-#         self.image_size = 32
-#
-#         weight1 = torch.normal(0, 0.02, size=(128, 1,5,5))
-#         weight1_bias =
-#         weight2 = torch.normal(0, 0.02, size=(128*2, 128,5,5))
-#         weight1_bias =
-#         weight3 = torch.normal(0, 0.02, size=(128 *4, 128 * 2, 5, 5))
-#         weight1_bias =
-#         weight4 = torch.normal(0, 0.02, size=(128 * 8, 128 * 4, 5, 5))
-#         weight1_bias =
-#
-#         linear_w =torch.normal(0, 0.1, size=(128 * 8, 128 * 4, 5, 5))
-#
-#         linear_b =
-#
-#
-#
-#
-#         reshaped_img = torch.reshape(a, [self.batch_size, 1, self.image_size[0], self.image_size[1]])
-#
-#         # weight = torch.Size([128, 1, 5, 5])
-#         weight = torch.randn(128, 1, 5, 5)
-#         h0 = F.conv2d(input, weight, padding=2, stride=2)
-#         # add bias term
-#         #torch.Size([64, 128, 16, 16])
-#
-#         weight2 = torch.randn(128*2, 128, 5, 5)
-#         h1 = F.conv2d(h0, weight2 , padding=2, stride=2)
-#
-#         weight3 = torch.randn(128*4, 128*2, 5, 5)
-#         h2 = F.conv2d(h1, weight3, padding=2, stride=2)
-#
-#         weight4 = torch.randn(128*8, 128*4, 5, 5)
-#         h3 = F.conv2d(h2, weight4, padding=2, stride=2)
-#
-#         h3 = h3.reshape([64,-1])
-#
-#         m = torch.nn.Linear(4096, 1)
-#
-#         h4 =m(h3)
-#
-#         return torch.sigmoid(h4), h4, h0
-#
-#         # weight for the 2d initializer=tf.truncated_normal_initializer(stddev=stddev)
-#         stddev = 0.02
-#
-#         # weight for linear w = tf.random_normal_initializer(mean=0.0, stddev=0.1)
-#
-#         # b  = tf.constant_initializer()
-#
-#         #  biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0)
-#
-#
-#
-#         # 64x32x32x1
-#
-#         # w = [5,5,1,128]
-#         # strides = [1,2,2,1]
-#         # TensorShape([64, 16, 16, 128])
-#
-#         # leakyrelu layer
-#         # # TensorShape([64, 16, 16, 128])
-#         F.conv2d(input, weights_torch, padding=2, stride=2)
-#
-#
-#         reshaped_img = tf.reshape(image, [self.batch_size, self.image_size[0], self.image_size[1], 1])
-#         # 64 x 32 x 32 x1
-#
-#         weights_tf.shape
-#         TensorShape([5, 5, 1, 128])
-#
-#         h0 = tf.nn.conv2d(x, weights_tf, strides=[1, 2, 2, 1], padding='SAME')
-#         #TensorShape([64, 16, 16, 128])
-#
-#         h0.get_shape()[-1] #- 128
-#         weight = [5,5,128,128*2]
-#         weights = torch.empty(5, 5, 128, 128 * 2)
-#         weights_tf = tf.convert_to_tensor(weights.numpy(), dtype=tf.float32)
-#
-#         h0 = ops.lrelu(self.d_bn0(h0, train))
-#         h1 = ops.conv2d(h0, self.d_size*2, name='d_h1_conv')
-#         h1 = ops.lrelu(self.d_bn1(h1, train))
-#         #TensorShape([64, 8, 8, 256])
-#
-#
-#         h2 = ops.conv2d(h1, self.d_size*4, name='d_h2_conv')
-#         weight = [5, 5, 256, 128 * 4]
-#         h2 = ops.lrelu(self.d_bn2(h2, train))
-#         #TensorShape([64, 4, 4, 512])
-#
-#
-#
-#        weight = [5,5,128*4, 128*8]
-#
-#         h3 = ops.conv2d(h2, self.d_size*8, name='d_h3_conv')
-#     # TensorShape([64, 2, 2, 1024])
-#
-#
-#         h3_tensor = ops.lrelu(self.d_bn3(h3, train))
-#
-#
-#
-#         h3 = tf.reshape(h3_tensor, [self.batch_size, -1])
-#     #TensorShape([64, 4096])
-#     # h3.get_shape()[1] = 4096
-#
-#     # weight = [4096, 1]
-#     # b = [1]
-#     # matmul(h4, w) + b
-#
-#
-#
-#         h4 = ops.linear(h3, h3.get_shape()[1], 1, scope='d_h5_lin')
-#     # 64 x 1
-#
-#     sigmoid(h4)
-#
-#
-# def truncated_normal_(tensor, mean=0, std=0.02):
-#     size = [128, 1, 5, 5]
-#     tmp = tensor.new_empty(size + (4,)).normal_()
-#     valid = (tmp < 2) & (tmp > -2)
-#     ind = valid.max(-1, keepdim=True)[1]
-#     tensor.data.copy_(tmp.gather(-1, ind).squeeze(-1))
-#     tensor.data.mul_(std).add_(mean)
-
-
-# image = np.random.uniform(-1, 1, [64, 32,32])
-# a = torch.FloatTensor(image)
-#
-# input = torch.randn(64, 32, 32, 1)
-# tf.reshape(image, [64, 32, 32, 1])
+    return input.mean(dim=2), input.std(dim=2)
--- train.py
+++ train.py
@@ -1,20 +1,12 @@
-from  prgan_discriminator import ShapeDiscriminator3D, l2, loss_discriminator, stat, loss_generator
-from prgan_generator import ShapeGenerator3D
+from  prgan_discriminator import ShapeDiscriminator3D, loss_discriminator
+from prgan_generator import ShapeGenerator3D, loss_generator
 import matplotlib.image as mpimg
-from torchvision import transforms
 import glob
 from torchvision.utils import save_image
-from PIL import Image
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-
 import os
-import shutil
 import argparse
 import numpy as np
-import random
-from skimage import measure
 import torch
 import torch.backends.cudnn as cudnn
 
@@ -26,12 +18,12 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def save_checkpoint(state, model, checkpoint_folder='checkpoints/'):
     if model == 'generator':
         filename = 'generator_best.pth.tar'
-    else:
+    elif model == 'discriminator':
         filename = 'discriminator_best.pth.tar'
+    else:
+        filename = 'generator_' + str(model) + '.pth.tar'
     checkpoint_file = os.path.join(checkpoint_folder, filename)
     torch.save(state, checkpoint_file)
-    # shutil.copyfile(checkpoint_file, os.path.join(checkpoint_folder, 'model_best.pth.tar'))
-
 
 def train(dataset_name, discriminator, generator, optimizer_Dloss, optimizer_Gloss):
     discriminator.train()  # switch to train mode
@@ -44,21 +36,16 @@ def train(dataset_name, discriminator, generator, optimizer_Dloss, optimizer_Glo
     # samepling
     batch_size=64
     z_size = 201
-    sample_z = np.random.uniform(-1, 1, [batch_size , z_size])
-    training_step = 0
 
     n_iterations = 200
 
     best_d_error = None
     best_g_error = None
 
-    # d_error = 9
-    #trans = transforms.Compose([transforms.Resize(32)])
     for epoch in range(n_iterations):
 
         # randomzied the file order
         rand_idxs = np.random.permutation(range(n_files))
-        #rand_idxs = np.random.permutation(128)
         # find the number of batches
         n_batches = n_files // batch_size
         #n_batches = 128 // batch_size
@@ -66,27 +53,19 @@ def train(dataset_name, discriminator, generator, optimizer_Dloss, optimizer_Glo
         for batch_i in range(n_batches):
             print(batch_i)
             # iterate through the files
-
-            #idxs_i = rand_idxs[batch_i * batch_size: (batch_i + 1) * batch_size]
             idxs_i = rand_idxs[batch_i * batch_size: (batch_i + 1) * batch_size]
             # load image batch
             imgs_batch = load_imgbatch(dataset_files[idxs_i], color=False)
             imgs_batch = torch.tensor(np.array(imgs_batch))
-            # downsample the image to 32 x 32
+
             tData = imgs_batch
 
             # fake data
             fake_image = generator.forward()
             fake_image = fake_image
-            # if d_error > 0.2:
-            #     print("train")
+
             d_error, d_pred_real, d_pred_fake = train_discriminator(optimizer_Dloss, tData, fake_image, discriminator)
             # 2. Train Generator
-            # Generate fake data
-            fake_data = generator.forward()
-            fake_image = fake_data
-
-            # Train G
             g_error = train_generator(optimizer_Gloss, fake_image, discriminator)
             print(g_error)
             print(d_error)
@@ -113,39 +92,29 @@ def train(dataset_name, discriminator, generator, optimizer_Dloss, optimizer_Glo
                 # Save 2D
                 save_image(fake_image, "fake" + str(epoch) + str(batch_i) + ".png")
                 save_image(tData, "real" +str(epoch)+ str(batch_i) + ".png")
-
                 # Save 3D
-                filled = generator.voxels[0] < 0.2
-                colors = np.array([0.4, 0.6, 0.8, 0.1])
-                edgecolors = np.array([0.0, 1.0, 0.0, 0.0])
-                fig = plt.figure()
-                ax = fig.gca(projection='3d')
-                ax.voxels(filled, facecolors=colors, edgecolors=edgecolors)
-                fig.savefig("3d" +str(epoch)+ ".png")
+                # filled = generator.voxels[0] < 0.2
+                # colors = np.array([0.4, 0.6, 0.8, 0.1])
+                # edgecolors = np.array([0.0, 1.0, 0.0, 0.0])
+                # fig = plt.figure()
+                # ax = fig.gca(projection='3d')
+                # ax.voxels(filled, facecolors=colors, edgecolors=edgecolors)
+                # fig.savefig("3d" +str(epoch)+ ".png")
+
+        save_checkpoint({"state_dict": generator.state_dict(), "optimizer": optimizer_Gloss.state_dict()},
+                                model=epoch)
 
 def train_discriminator(optimizer, real_data, fake_data, discriminator):
-    # Reset gradients
-    # optimizer.zero_grad()
 
-    # 1.1 Train on Real Data
     prediction_real = discriminator(real_data)
-    # Calculate error and backpropagate
-    #error_real = loss(prediction_real, torch.ones(prediction_real.shape))
-    #error_real.backward()
 
-    # 1.2 Train on Fake Data
     prediction_fake = discriminator(fake_data)
     error = loss_discriminator(prediction_real,prediction_fake)
-    # Calculate error and backpropagate
-    #error_fake = loss(prediction_fake, torch.zeros(prediction_fake.shape))
-    #error_fake.backward()
 
     if error > 0.2:
         print("Train discriminator")
         optimizer.zero_grad()
         error.backward(retain_graph=True)
-
-        # 1.3 Update weights with gradients
         optimizer.step()
 
     # Return error and predictions for real and fake inputs
@@ -165,87 +134,36 @@ def train_generator(optimizer, fake_image, discriminator):
         # Return error
     return error
 
+def generate_3d(generator):
+    fake_image = generator.forward()
+    save_image(fake_image, "fake.png")
+    # Save 3D
+    filled = generator.voxels[0] < 0.2
+    colors = np.array([0.4, 0.6, 0.8, 0.1])
+    edgecolors = np.array([0.0, 1.0, 0.0, 0.0])
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.voxels(filled, facecolors=colors, edgecolors=edgecolors)
+    fig.savefig("3d_object.png")
 
-
-#
-#
-# # validation function
-# def val(dataset, model, optimizer, args):
-#     model.eval()  # switch to test mode
-#     loss_count = 0.0
-#     num_batch = len(dataset)
-#     loss = 0
-#     for i in range(num_batch):
-#         data = dataset[i]  # a dict
-#         xyz_tensor = data['xyz'].to(device)
-#         ground_truth = data['gt_sdf'].to(device)
-#         with torch.no_grad():
-#             pred_sdf_tensor = model(xyz_tensor)
-#             predict = torch.clamp(pred_sdf_tensor, -args.clamping_distance, args.clamping_distance)
-#             truth = torch.clamp(ground_truth, -args.clamping_distance, args.clamping_distance)
-#         loss = loss + (abs(predict - truth)).sum()
-#         loss_count = loss_count + 1
-#
-#     avg_loss = loss /loss_count
-#
-#
-#     return avg_loss
-#
-#
-#
-# # testing function
-# def test(dataset, model, args):
-#     model.eval()  # switch to test mode
-#     num_batch = len(dataset)
-#     number_samples = dataset.number_samples
-#     grid_shape = dataset.grid_shape
-#     IF = np.zeros((number_samples, ))
-#     start_idx = 0
-#     for i in range(num_batch):
-#         data = dataset[i]  # a dict
-#         xyz_tensor = data['xyz'].to(device)
-#         this_bs = xyz_tensor.shape[0]
-#         end_idx = start_idx + this_bs
-#         with torch.no_grad():
-#             pred_sdf_tensor = model(xyz_tensor)
-#             pred_sdf_tensor = torch.clamp(pred_sdf_tensor, -args.clamping_distance, args.clamping_distance)
-#         pred_sdf = pred_sdf_tensor.cpu().squeeze().numpy()
-#         IF[start_idx:end_idx] = pred_sdf
-#         start_idx = end_idx
-#     IF = np.reshape(IF, grid_shape)
-#     verts, simplices = measure.marching_cubes_classic(IF, 0)
-#     x, y, z = zip(*verts)
-#     colormap = ['rgb(255,105,180)', 'rgb(255,255,51)', 'rgb(0,191,255)']
-#     fig = ff.create_trisurf(x=x,
-#                             y=y,
-#                             z=z,
-#                             plot_edges=False,
-#                             colormap=colormap,
-#                             simplices=simplices,
-#                             title=args.graphname)
-#     plotly.offline.plot(fig, filename=args.save_file_name)
-#     return
-#
 
 def main(args):
-    best_loss = 2e10
-    best_epoch = -1
 
     discriminator = ShapeDiscriminator3D()
     generator = ShapeGenerator3D()
 
     optimizer_Dloss = torch.optim.Adam(filter(lambda p: p.requires_grad, discriminator.parameters()), lr=0.00001, weight_decay=0.5)
-    optimizer_Gloss = torch.optim.Adam(filter(lambda p: p.requires_grad, generator.parameters()), lr=0.0025, weight_decay=0.05)
+    optimizer_Gloss = torch.optim.Adam(filter(lambda p: p.requires_grad, generator.parameters()), lr=0.00025, weight_decay=0.5)
 
     if args.evaluate:
         print("\nEvaluation only")
-        path_to_resume_generator ='checkpoints/generator_best.pth.tar'
+        path_to_resume_generator ='checkpoints/generator_best.pth(1).tar'
         print("=> Loading best generator '{}'".format(path_to_resume_generator))
         generator_checkpoint = torch.load(path_to_resume_generator)
         generator.load_state_dict(generator_checkpoint['state_dict'])
         print("evaluate generator")
         # Generate 3d shape
-        # 3d
+        generate_3d(generator)
         return
 
     if args.retrain:
